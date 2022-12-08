1f454353c3c791d280edb1b9141c09d054e87f1b
Neena Augustine -X (neaugust - GENPACT INDIA at Cisco) <neaugust@cisco.com>
2022-12-08 12:00:39 +0530

    pushing the last commit

--- src/main/java/com/community/pages/CommunityHomePage.java
+++ src/main/java/com/community/pages/CommunityHomePage.java
@@ -6,10 +6,10 @@ import com.community.enums.Waits;
 
 public class CommunityHomePage extends Commons {
 	
-	private String  linkMenu="//button/span[text()='%replace%']";
+	private String  linkMenu="//button/span[text()='%s']";
 	
 	public  void clickOnMenu(String menuitem) throws Exception {
-		String path=linkMenu.replace("%replace%",menuitem);
+		String path=String.format(linkMenu, menuitem);
 		click(By.xpath(path), Waits.CLICKABLE, menuitem);
 		
 	}
--- src/main/java/com/community/pages/CommunityNavigation.java
+++ src/main/java/com/community/pages/CommunityNavigation.java
@@ -6,26 +6,26 @@ import com.community.enums.Waits;
 
 public class CommunityNavigation extends Commons {
 
-private String  linkSubMenu="//a[text()='%replacable%']";
-private String linkDiscussForum="//div[@class='tier3-container --active']/div//a[text()='%replacable%']";
-private String linkResource="//div[@class='tier3-container --active']//div//a[text()='%replacable%']";
+private String  linkSubMenu="//a[text()='%s']";
+private String linkDiscussForum="//div[@class='tier3-container --active']/div//a[text()='%s']";
+private String linkResource="//div[@class='tier3-container --active']//div//a[text()='%s']";
 	
 	public  CommunityNavigation clickOnSubMenu(String submenuitem) throws Exception {
-		String path=linkSubMenu.replace("%replacable%",submenuitem);
+		String path=String.format(linkSubMenu, submenuitem);
 		click(By.xpath(path), Waits.CLICKABLE, submenuitem);
 		return this;
 		
 	}
 	
 	public  CommunityConversation clickOnDiscussForum(String menuitem) throws Exception {
-		String path=linkDiscussForum.replace("%replacable%",menuitem);
+		String path=String.format(linkDiscussForum, menuitem);
 		click(By.xpath(path), Waits.CLICKABLE, menuitem);
 		return new CommunityConversation();
 		
 	}
 	
 	public  CommunityConversation clickOnResources(String menuitem) throws Exception {
-		String path=linkResource.replace("%replacable%",menuitem);
+		String path=String.format(linkResource, menuitem);
 		click(By.xpath(path), Waits.CLICKABLE, menuitem);
 		return new CommunityConversation();
 	}
